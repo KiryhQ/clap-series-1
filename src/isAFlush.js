bc537561f704ff97e64ce202c6c6814838a6fad0
KiryhQ <quentin.dereumaux@gmail.com>
2020-06-08 16:20:41 +0200

    plusArray done

--- src/isAFlush.js
+++ src/isAFlush.js
@@ -10,8 +10,38 @@
  * - Le type de la carte : d => ♦, c => ♣, h => ♥, s => ♠
  */
 
-function isAFlush(cards) {
-  // CODE HERE
-}
 
+// TO DO use regexp to check if there's 5 time the same match on an array
+// const cards = ["As", "10s", "Js", "Qs", "Ah", "2s", "3c"];
+// let i= 0;
+// function isFlushed(cards){
+
+//   cards.forEach(element => {
+//      if(element.match(/s/)){
+//        i+=1;
+//      }
+//   });
+  
+//   return i;
+// }
+
+// console.log(isFlushed(cards));
+
+
+function isAFlush(cards) {
+
+  let signs = ['d','c','h','s'];
+  let result = [];
+  let flush = false;
+
+  for(let card in cards){
+    result.push(cards[card].slice(-1));
+  }  
+
+  for(let sign in signs){
+    if(result.filter(value => value == signs[sign]).length >= 5) flush = true;
+  }
+
+  return flush;
+}
 export { isAFlush };
--- src/plusArray.js
+++ src/plusArray.js
@@ -6,7 +6,18 @@
  * alors plusArray(arr1, arr2) retournera 30 
  */
 function plusArray(arr1, arr2) {
-  // CODE HERE
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    
+   if (arr1.length < 1 || arr2.length < 1){
+      if(arr1.length > 1) {
+        return arr1.reduce(reducer);
+      } else {
+        return arr2.reduce(reducer);
+      }
+  } else {
+    return (arr1.reduce(reducer)) + (arr2.reduce(reducer));
+  }
+  
 }
 
 export { plusArray };
